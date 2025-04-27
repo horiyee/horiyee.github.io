c54843b19c2be62daa56defeef1d8fe6a084ede7
horiyee <kaito@horiyee.jp>
2025-04-28 01:30:28 +0900

    feat: introduce MDX

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   output: "export",
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
