dea05fe46c65eeaa42d60ce12491cc9c42e99f95
horiyee <kaito@horiyee.jp>
2025-04-28 01:30:59 +0900

    feat: add pages to /posts

---
+++ src/app/posts/311ze4r8ztg3/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import Markdown from "../../../markdown/posts/311ze4r8ztg3.mdx";
+
+const Page: React.FC = () => {
+  return <Markdown />;
+};
+
+export default Page;
---
+++ src/app/posts/lsk8k8gpz/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import Markdown from "../../../markdown/posts/lsk8k8gpz.mdx";
+
+const Page: React.FC = () => {
+  return <Markdown />;
+};
+
+export default Page;
