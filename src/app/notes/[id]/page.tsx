bba3f10f6cb4166fd92f55da7db19d6ee51d5bdf
horiyee <kaito@horiyee.jp>
2025-04-28 01:58:42 +0900

    feat: add /notes

---
+++ src/app/notes/[id]/page.tsx
@@ -0,0 +1,22 @@
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+const Page: React.FC<Props> = async (props) => {
+  const { params } = props;
+  const { id } = await params;
+
+  const { default: Post } = await import(`../../../markdown/notes/${id}.mdx`);
+
+  return <Post />;
+};
+
+export const generateStaticParams = async () => {
+  return [
+    {
+      id: "test",
+    },
+  ];
+};
+
+export default Page;
