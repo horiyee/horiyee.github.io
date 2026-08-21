0bbd59cbfc43bafba1281c01a088e76c04719c34
Kaito Horiuchi <horiyee.ka@gmail.com>
2026-08-22 02:51:28 +0900

    Revert "Add About page with migrated profile content"

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,8 @@
 import { Hono } from 'hono'
 import { HomePage } from './pages/HomePage'
-import { AboutPage } from './pages/AboutPage'
 
 const app = new Hono()
 
 app.get('/', (c) => c.html(<HomePage />))
-app.get('/about/', (c) => c.html(<AboutPage />))
 
 export default app
